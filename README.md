1769dde2455fcb7f26dd0f4d838f2cb66b85f196
Oliver King <kingoliver@microsoft.com>
2022-07-15 10:25:42 -0400

    add starter code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # notary-sign
+
 GitHub action to sign images with Notary specification.
---
+++ src/run.ts
@@ -0,0 +1,7 @@
+import * as core from "@actions/core"
+
+
+export async function run() {
+}
+
+run().catch(core.setFailed)
